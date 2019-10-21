fb3425582ca797567d376ed70c3a7c37849c2dc3
dhruvgami <dhruvgami@gmail.com>
2019-10-21 00:37:29 -0400

    Added connection to datocms; fetched one festival

--- src/components/Festivals/Festivals.js
+++ src/components/Festivals/Festivals.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import ContentWrapper from '../Layout/ContentWrapper';
 
+require('babel-polyfill');
+
+const SiteClient = require('datocms-client').SiteClient;
+const client = new SiteClient('7ace633fda9abdfe515bb8b5ed7639');
+var festival = String();
+client.items.all({'filter[type]': 'festival'}, {allPages: true})
+                .then((models) => console.log(models));
+
 class Festivals extends Component {
+    constructor() {
+        super();
+        this.state = {
+            // TODO: Change this to hold the entire array of results and retrieve into a list on the page
+            festivalName: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getFestivals().then(result => this.setState({
+            festivalName: result[0].name
+        }))
+    }
+
+    getFestivals() {
+        return client.items.all({'filter[type]': 'festival'}, {allPages: true});
+    }
+
     render() {
         return (
             <ContentWrapper>
                 {/* view content here */}
                 <h1>Festivals</h1>
+                <h3> - {this.state.festivalName} - </h3>
             </ContentWrapper>
         );
     }
